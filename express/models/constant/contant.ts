7ffea86df7149244e157d08a3ca929ce165a4984
fahust <kevin.dellova@hotmail.fr>
2022-10-31 00:37:32 +0100

    do job

---
+++ express/models/constant/contant.ts
@@ -0,0 +1,8 @@
+import { Item } from "../../enums/enum";
+
+export const itemsJobs = {
+    MINER:[Item.STONE,Item.IRON],
+    SMITHY : [Item.SHIELD],
+    FISHER : [Item.FISH],
+    HARVEST : [Item.MUSHROOM],
+}

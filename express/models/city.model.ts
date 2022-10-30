fc353b5d8eaa4d182443f12ff17251c948ad5fde
fahust <kevin.dellova@hotmail.fr>
2022-10-30 23:42:33 +0100

    put items in city and clear inventory of user

--- express/models/city.model.ts
+++ express/models/city.model.ts
@@ -1,11 +1,14 @@
 import mongoose, { Schema, Document } from "mongoose";
+import { Item } from "../enums/enum";
 
 export interface ICity extends Document {
   name: string;
+  items: Record<Item, number>;
 }
 
 const CitySchema: Schema = new Schema({
   name: { type: String, required: true },
+  items: { type: Schema.Types.Mixed, required: true, default: {} },
 });
 
 export default mongoose.model<ICity>("City", CitySchema);
